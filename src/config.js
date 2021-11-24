8629463add1b892cff6b62b7a77b4b4461a9dc1b
ColorsTheForce <66538158+colorstheforce@users.noreply.github.com>
2021-11-23 20:13:09 -0500

    added Assets
    assets updated

--- src/config.js
+++ src/config.js
@@ -22,6 +22,6 @@ const rarity = [
     { key: "_sr", val: "super rare" },
 ];
 
-const defaultEdition = 5;
+const defaultEdition = 20;
 
 module.exports = { layersOrder, format, rarity, defaultEdition };
